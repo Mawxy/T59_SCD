1f3c75e020f77edd4269213190df17f29d51424d
Maxwell Walker <mjwalker@mtu.edu>
2021-11-08 01:54:20 -0500

    Added basic csv creation

--- Python/SerialReader.py
+++ Python/SerialReader.py
@@ -1,14 +1,19 @@
 import serial
 import time
 
+dirPath = ".data/"
 timeStr = time.strftime("%Y_%m_%d_%H_%M")
 
 arduinoPort = "COM3"
 baud = 9600
-fileName = timeStr + "_TestBenchData.csv"
+fileName = dirPath + timeStr + "_TestBenchData.csv"
 # print(fileName)
 
-ser = serial.Serial(arduinoPort, baud)
+#ser = serial.Serial(arduinoPort, baud)
 
 print("Connect to Arduino port: " + arduinoPort)
 
+file = open(fileName, "a")
+print("Created file")
+
+file.close()
