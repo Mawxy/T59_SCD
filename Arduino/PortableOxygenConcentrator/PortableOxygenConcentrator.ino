f0a03ab2c3330ef397cd5416726eb9da399f5436
Maxwell Walker <mjwalker@mtu.edu>
2021-11-09 16:31:02 -0500

    Properly creates CSV using data from humidity sensors

--- Arduino/PortableOxygenConcentrator/PortableOxygenConcentrator.ino
+++ Arduino/PortableOxygenConcentrator/PortableOxygenConcentrator.ino
@@ -9,71 +9,114 @@
 #define twoWay_column1   5 
 #define twoWay_column2   6 
 #define inletHumidityAddr 0x45
-#define columnUptime (10 * 1000) //first number is the number of seconds desired
+#define outletHumidityAddr 0x44
+#define columnUptime 10 //(in seconds)
 
 
 Adafruit_SHT31 inletHumidity = Adafruit_SHT31();
+Adafruit_SHT31 outletHumidity = Adafruit_SHT31();
 
-void setup() {
+bool inletHumidityEnabled = true;
+bool outletHumidityEnabled = true;
+int loopT = 0;
+
+
+void setup() 
+{
   Serial.begin(9600);
 
-//  while(!Serial)
-//    delay(10);
+  while(!Serial)
+    delay(10);
 
-//  if( !inletHumidity.begin(inletHumidityAddr) )
-//  {
-//    Serial.println("Could not find inlet humidity sensor");
-//    while(1) delay(1);
-//  }
-//
-//  if (inletHumidity.isHeaterEnabled())
-//  {
-//    inletHumidity.heater(false);
-//  }
+  if( !inletHumidity.begin(inletHumidityAddr))
+  {
+    Serial.println("Continuing without silica inlet humidity and temperature sensor");
+    inletHumidityEnabled = false;
+    
+  }
+  
+  if (!outletHumidity.begin(outletHumidityAddr))
+  {
+    Serial.println("Continuing without silica outlet humidity and temperature sensor");
+    outletHumidityEnabled = false;
+  }
+
   
   pinMode(threeWay_column1, OUTPUT);
   pinMode(threeWay_inlet,   OUTPUT);
   pinMode(threeWay_column2, OUTPUT);
   pinMode(twoWay_column1,   OUTPUT);
   pinMode(twoWay_column2,   OUTPUT);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void sensorLoop()
+{
+  float inletT = -1;
+  float inletH = -1;
+  float outletT = -1;
+  float outletH = -1;
   
+  for(int i = 0; i < columnUptime; i++)
+  {
+    if(inletHumidityEnabled)
+    {
+      inletT = inletHumidity.readTemperature();
+      inletH = inletHumidity.readHumidity();
+    }
+
+    if(outletHumidityEnabled)
+    {
+      outletT = outletHumidity.readTemperature();
+      outletH = outletHumidity.readHumidity();
+    }
+
+    Serial.print(loopT);
+    Serial.print(",");
+    
+    if (!isnan(inletT) || !isnan(inletH)) {  // check if 'is not a number'
+      Serial.print(inletT); Serial.print(","); Serial.print(inletH);Serial.print(",");
+    } else { 
+      Serial.print("-1"); Serial.print(","); Serial.print("-1"); Serial.print(",");
+    }
+
+    if (!isnan(outletT) || !isnan(outletH)) {  // check if 'is not a number'
+      Serial.print(outletT); Serial.print(","); Serial.print(outletH);
+    } else { 
+      Serial.print("-1"); Serial.print(","); Serial.print("-1");
+    }
+
+    Serial.println("");
+    loopT = loopT + 1;
+    delay(1000);
+  }
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  if(loopT == 0)
+  {
+    delay(10000);
+    Serial.println("Time,inletT,inletH,outletT,outletH");
+  }
+
   
   digitalWrite(threeWay_column1,  LOW); //needs to be low
   digitalWrite(threeWay_inlet,   LOW); 
   digitalWrite(threeWay_column2,  LOW);
   digitalWrite(twoWay_column1,   LOW); //closed
   digitalWrite(twoWay_column2,    HIGH); //open
-  Serial.println("COLUMN 1 HIGH");
   
-//  for(int i = 0; i < columnUptime; i++)
-//  {
-//    float inletT = inletHumidity.readTemperature();
-//    float inletH = inletHumidity.readHumidity();
-//
-//    if (!isnan(inletT) || !isnan(inletH) {  // check if 'is not a number'
-//      Serial.print("Temp *C = ");
-//      Serial.print(inletT);
-//      Serial.print("\t\t");
-//      Serial.print("Hum. % = ");
-//      Serial.println(inletH);
-//    } else { 
-//      Serial.println("Failed to read temperature");
-//    }
-//
-//    delay(1000);
-//  }
-  delay(columnUptime);
+  sensorLoop();
   
   digitalWrite(threeWay_column1,  HIGH);
   digitalWrite(threeWay_inlet,    HIGH);
   digitalWrite(threeWay_column2, HIGH);
   digitalWrite(twoWay_column1,    HIGH);
   digitalWrite(twoWay_column2,   LOW);
-  Serial.println("COLUMN 2 HIGH");
-  delay(columnUptime);
+  
+  sensorLoop();
 
 }
--- Python/SerialReader.py
+++ Python/SerialReader.py
@@ -9,11 +9,27 @@ baud = 9600
 fileName = dirPath + timeStr + "_TestBenchData.csv"
 # print(fileName)
 
-#ser = serial.Serial(arduinoPort, baud)
+samples = 61
+line = 0
+
+ser = serial.Serial(arduinoPort, baud)
 
 print("Connect to Arduino port: " + arduinoPort)
 
 file = open(fileName, "a")
 print("Created file")
 
+while line <= samples:
+    getData = str(ser.readline())
+    data = getData[2:][:-5]
+    print(data)
+
+    file.write(data)
+    file.write('\n')
+
+    line = line + 1
+
+print("Collection done")
+
+ser.close()
 file.close()
